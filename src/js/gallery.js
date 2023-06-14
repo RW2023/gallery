eb5f641ffd62ebfeb179ee02e844c224884ba6f1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-06-14 03:50:41 -0400

    add and change images. add icons

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -20,10 +20,34 @@
         reset: true,
     });
 
-anime({
-    targets: '#flower',
-    translateX: 250,
-    backgroundColor: '#000', // change the color to black
-    duration: 2000,
-    easing: 'easeInOutSine'
-});
+// Define the anime.js animation
+function dropBounceImage(img) {
+    anime.timeline({
+        targets: img,
+        easing: 'easeOutBounce'
+    })
+        .add({
+            translateY: [-500, 0], // Drop from above
+            duration: 1000
+        })
+        .add({
+            scale: [1, 1.1, 1], // Slight bounce effect
+            duration: 500,
+        });
+}
+// Image Bounce Effect
+// Set up the intersection observer
+// let observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             dropBounceImage(entry.target); // If the image is in the viewport, drop and bounce the image
+//             observer.unobserve(entry.target); // Stop observing the image after the animation has been triggered
+//         }
+//     });
+// }, { threshold: 0.1 }); // Threshold: 10% - start the animation when 10% of the image is visible
+
+// // Observe each image
+// document.querySelectorAll('img[data-src]').forEach(img => {
+//     observer.observe(img);
+// });
+
